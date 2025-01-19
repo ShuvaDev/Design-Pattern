57e22d8bf6a73b5755a0f0e79f38b61b576aa506
Shuva Dev <s.r.shuvadeb@gmail.com>
2025-01-19 12:59:09 +0600

    Prototype Design Pattern

---
+++ Prototype/Person.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prototype
+{
+    public class Person : ICloneable
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public Address Address { get; set; }
+        public Person()
+        {
+            Address = new Address();
+        }
+        public object Clone()
+        {
+            Person person = MemberwiseClone() as Person;
+
+            person.Address = new Address();
+            person.Address.Street = this.Address.Street;
+            return person;
+        }
+    }
+}
---
+++ Prototype/Program.cs
@@ -0,0 +1,12 @@
+﻿using Prototype;
+
+Person person1 = new Person();
+person1.Name = "Shuva Dev";
+person1.Email = "s.r.shuvadeb@gmail.com";
+person1.Address.Street = "Chattogram";
+
+Person person2 = person1.Clone() as Person;
+person2.Address.Street = "Dhaka";
+
+Console.WriteLine($"Person 1 : {person1.Address.Street}");
+Console.WriteLine($"Person 2 : {person2.Address.Street}");
